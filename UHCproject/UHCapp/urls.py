1a69ee4d840cae9d90efc5a675dd6d082c6f143f
stancrypt <nnamdiogbonna93@gmail.com>
2025-02-12 17:22:05 +0100

    added forms

--- UHCproject/UHCapp/urls.py
+++ UHCproject/UHCapp/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('about/', views.about, name='about'),
-    path('chart/', views.charts, name='chart')
+    path('chart/', views.charts, name='chart'),
+    path('form/', views.get_name, name='form'),
+    path('model/', views.model, name='model')
+
    
 ]
--- UHCproject/UHCapp/views.py
+++ UHCproject/UHCapp/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from UHCapp.chart import calculate
+from django.shortcuts import render, redirect 
+from django.http import HttpResponse, HttpResponseRedirect
+from .chart import calculate
+from .form import NameForm
+
+from .models import Person
 
 
 # Create your views here.
@@ -16,6 +19,30 @@ def dashboard(request):
 def about(request):
     return render(request, 'uhc/about.html', )
 
+def model(request):
+    person=Person()
+    return render(request, 'uhc/models.html', {'person':person} )
+
 def charts(request):
     cha= calculate()
     return render(request, 'uhc/chart.html', {'cha':cha})
+
+
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect("/thanks/")
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, "uhc/form.html", {"form": form})
