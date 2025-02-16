4b8925809101f74796408647a6f020cc0694165f
stancrypt <nnamdiogbonna93@gmail.com>
2025-02-16 14:51:41 +0100

    service

--- UHCproject/UHCapp/models.py
+++ UHCproject/UHCapp/models.py
@@ -12,3 +12,14 @@ class FormResponse(models.Model):
 
     def __str__(self):
         return f"{self.user_name} - {self.subject}"
+
+class ServiceProvicers(models.Model):
+    user_name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    sender = models.EmailField(max_length=50)
+    c_myself = models.BooleanField(default=False)
+    submitted_at = models.DateTimeField(auto_now_add=True)  # Tracks submission time
+
+    def __str__(self):
+        return f"{self.user_name} - {self.subject}"
